8ce8d95f51042f8962f46e76bc34e042c2d22351
DyneStein <dyssart@gmail.com>
2024-11-24 19:35:47 +0500

    file.cpp
    mene teri file bigar di hai

--- file.cpp
+++ file.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
+
     cout<<"HELLo";
+    cout<<"Sprise mother father";
+    cout<<"Shehryar has a small PP";
+    cout<<"Shehryar ka level hai";
+
     return 0;
 }
